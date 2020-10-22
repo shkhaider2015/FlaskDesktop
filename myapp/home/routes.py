6f66129d3520baa2e0b11e8de29fd04828fe563a
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-10-22 16:44:06 +0500

    wqew

--- myapp/home/routes.py
+++ myapp/home/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 Home = Blueprint('Home', __name__)
 
 @Home.route('/')
 @Home.route('/home')
 def method_name():
-   return("Home work !!")
+   return render_template("testing.html")
